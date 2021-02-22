3c391ccf4c91a42030fa98bf371f2e69fc2b85ba
David Mulholland <davidmulholland@Davids-iMac.local>
2021-02-22 13:11:25 +0000

    sss

--- app.js
+++ app.js
@@ -66,9 +66,15 @@ app.get('/', (req, res) => {
 app.use(express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', (req, res) => {
-  res.status(404).json('Sorry cant find that route');
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  console.log('dirname', __dirname);
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+}
 
 const port = process.env.PORT || 5000;
 
