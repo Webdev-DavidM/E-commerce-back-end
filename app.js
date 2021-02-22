57796d3ec14f8b37e8a2fd8b8e555692c86a49f3
David Mulholland <davidmulholland@Davids-iMac.local>
2021-02-22 13:56:01 +0000

    sss

--- app.js
+++ app.js
@@ -66,17 +66,16 @@ app.get('/', (req, res) => {
 app.use(express.static(path.join(__dirname, 'uploads')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '..', '/front-end/build')));
+app.use(express.static(path.join(__dirname, '..', '/front-end/build')));
+let dirname = console.log(
+  path.resolve('dirname', __dirname, '..', '/front-end/build')
+);
 
-  console.log('dirname', __dirname, '..', '/front-end/build');
-
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '..', 'front-end', 'build', 'index.html')
-    )
-  );
-}
+app.get('*', (req, res) =>
+  res.sendFile(
+    path.resolve(__dirname, '..', 'front-end', 'build', 'index.html')
+  )
+);
 
 const port = process.env.PORT || 5000;
 
