a9e60de3bfe2c4a75520d0495b84502a9529967c
David Mulholland <davidmulholland@Davids-iMac.local>
2021-03-04 17:52:06 +0000

    back-end set up with multer to accept created products by administrator from the front end

--- routes/adminuser.js
+++ routes/adminuser.js
@@ -4,8 +4,14 @@ import jwt from 'jsonwebtoken';
 import jwtVerify from '../middleware/jwtVerify.js';
 import expressValidator from 'express-validator';
 const { body, validationResult } = expressValidator;
+import Product from '../models/Products.js';
 
 import User from '../models/User.js';
+
+import multer from 'multer';
+// var upload = multer({ dest: 'uploads/' });
+
+import fileUpload from '../middleware/file-uploads.js';
 
 // Note all admin routes are protected routes so I have chosen to store in them in a separate route from the
 // users, I have added middleware to check there is a valid JWT token before adding the user. I will also check that the
@@ -13,20 +19,21 @@ import User from '../models/User.js';
 // middleware called productDetailsCorrect to make sure that all the data required has been provided
 
 /* POST route- this will create a new product in the database, */
-// Below I am checking which category it is for so I know which model to add the product to
 
 app.post(
   '/create',
+  fileUpload.single('image'),
   jwtVerify,
+
   body('name', 'Name is required').not().isEmpty(),
   body('price').isInt().not().isEmpty(),
   body('description', 'Description is required').not().isEmpty(),
-  body('images', 'Image path is required').not().isEmpty(),
   body('stock').isInt().not().isEmpty(),
   body('size', 'Size is required').not().isEmpty(),
-  body('reviews').isArray(),
+  body('category', 'Category is required').not().isEmpty(),
   body('subcategory', 'Subcategory is required').not().isEmpty(),
   async (req, res) => {
+    // console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
@@ -41,7 +48,36 @@ app.post(
             .json('You dont have authority to make these changes')
             .end();
         } else if (adminUser.admin === true) {
-          // route to be completed when I have completed the admin section
+          let {
+            name,
+            description,
+            size,
+            brand,
+            colour,
+            category,
+            subcategory,
+          } = req.body;
+
+          let stock = parseInt(req.body.stock);
+          let price = parseInt(req.body.price);
+
+          let newProduct = await new Product({
+            name,
+            price: price,
+            description,
+            images: [`/uploadedimages/${req.imageId}`],
+            stock: stock,
+            size,
+            brand,
+            colour,
+            category,
+            reviews: [],
+            subcategory,
+          });
+          let createdProduct = await newProduct.save();
+          if (createdProduct) {
+            res.status(201).json(createdProduct);
+          }
         }
       }
     } catch (err) {
@@ -55,21 +91,23 @@ app.post(
 app.put(
   '/update/:productId',
   jwtVerify,
-  body('name', 'Name is required').isString().not().isEmpty(),
-  body('price').isInt().not().isEmpty(),
-  body('description', 'Description is required').isString().not().isEmpty(),
-  body('images', 'Image path is required').isString().not().isEmpty(),
-  body('stock').isInt().not().isEmpty(),
-  body('size', 'Size is required').not().isEmpty(),
-  body('reviews').isArray(),
-  body('subcategory', 'Subcategory is required').isString().not().isEmpty(),
+
+  // body('name', 'Name is required').isString().not().isEmpty(),
+  // body('price').isInt().not().isEmpty(),
+  // body('description', 'Description is required').isString().not().isEmpty(),
+  // body('images', 'Image path is required').isString().not().isEmpty(),
+  // body('stock').isInt().not().isEmpty(),
+  // body('size', 'Size is required').not().isEmpty(),
+  // body('reviews').isArray(),
+  // body('subcategory', 'Subcategory is required').isString().not().isEmpty(),
   async (req, res) => {
     const { productId } = req.params;
+    console.log(req.body);
     const { email } = req;
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+    // if (!errors.isEmpty()) {
+    //   return res.status(400).json({ errors: errors.array() });
+    // }
     try {
       let adminUser = await User.findOne({ email });
 
@@ -91,27 +129,32 @@ app.put(
 
 /* PUT route- this will update an existing product in the database, */
 
-app.delete('/delete/:productId', jwtVerify, async (req, res) => {
-  const { productId } = req.params;
-  const { email } = req;
-  try {
-    let adminUser = await User.findOne({ email });
+app.delete(
+  '/delete/:productId',
+  jwtVerify,
 
-    if (adminUser) {
-      if (adminUser.admin === false) {
-        res
-          .status(401)
-          .json('You dont have authority to make these changes')
-          .end();
-      } else if (adminUser.admin === true) {
-        switch (req.headers.category) {
-          case 'cycle':
+  async (req, res) => {
+    const { productId } = req.params;
+    const { email } = req;
+    try {
+      let adminUser = await User.findOne({ email });
+
+      if (adminUser) {
+        if (adminUser.admin === false) {
+          res
+            .status(401)
+            .json('You dont have authority to make these changes')
+            .end();
+        } else if (adminUser.admin === true) {
+          switch (req.headers.category) {
+            case 'cycle':
+          }
         }
       }
+    } catch (err) {
+      console.log(err);
     }
-  } catch (err) {
-    console.log(err);
   }
-});
+);
 
 export default app;
